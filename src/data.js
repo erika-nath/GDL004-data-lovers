18882a607868b9ff44f26b82323985cce4c416e8
unknown <eugynias>
2019-11-19 11:54:30 -0500

    Comienzo filter

--- src/data.js
+++ src/data.js
@@ -1,7 +1,9 @@
-/* Manejo de data */
 
-// esta es una función de ejemplo
 
+
+  /* Manejo de data */
+
+  // esta es una función de ejemplo
 export const example = () => {
   return 'example';
 };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+ import POKEMON from './data/pokemon/pokemon.js'
 
-import { example } from './data.js';
 
-console.log(example);
+console.log(POKEMON);
 
+
+
+  //  document.getElementById('filtrarLetra').style.display = "none";
+  //  document.getElementById('home').style.display = 'block';
+//}
+//document.getElementById('homeBtn').addEventListener('click', homeIcon);
+
+//funcion para cambiar a la pantalla de comparar al darle click en 'compareBtn' se encuentran los selectores
+//function compare() {
+    //document.getElementById('home').style.display = "none"; //darle invisibilidad
+    //document.getElementById('filtrarLetra').style.display = "block"; //mostrar el bloque (pantalla/slide)
+//}
+//document.getElementById('pokebolaPoder').addEventListener('click', compare);
 /*
  * console.log(POKEMON);
  * console.log(LoL);
