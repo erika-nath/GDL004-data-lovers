96bb3355bbdcd941d5e85b0061a3635793898cc7
Erika <enath6.25@gmail.com>
2019-11-22 11:29:47 -0600

    filtrado y muestra grass

--- src/data.js
+++ src/data.js
@@ -1,12 +1,5 @@
-/* Manejo de data
-
-// esta es una función de ejemplo
-
-export const example = () => {
-  return 'example';
-};*/
-
-export const filterCandy = (Pokemon) => {
+//ESTO ES LO DE ANGIE  NO BORRAR DE 7 A 19
+/*export const filterCandy = (Pokemon) => {
 const candy= Pokemon.filter( (pkm) => {
   for(let i=0;i<pkm.type.length;i++){
   if(pkm.type[i] === 'Grass'){
@@ -17,16 +10,30 @@ const candy= Pokemon.filter( (pkm) => {
 console.log(candy)
 return candy;
 
+};*/
+
+
+//en pkm se guardan los 14 pokemones ya filtrados
+export const filterType = (Pokemon) => {
+const type= Pokemon.filter( (pkm) => {
+  for(let i=0;i<pkm.type.length;i++){
+  if(pkm.type[i] === 'Grass'){
+      return pkm;
+    }
+  }
+});
+console.log(type)
+return type;
 };
 
 
 
-/*export const filterItems=(Pokemon)=>{
-const filterItems= query => {
-  return Pokemon.filter((el) =>
-    el.toLowerCase().indexOf(query.toLowerCase()) > -1);
-}
-};//
 
-//console.log(Pokemon('name')); //
-console.log(filterItems('water')) //*/
+
+
+
+
+//document.getElementById("demo").innerHTML = allweaknesses;
+//};
+
+//};
--- src/main.js
+++ src/main.js
@@ -1,38 +1,22 @@
 
+import POKEMON from './data/pokemon/pokemon.js'
+console.log(POKEMON);
+
 import Pokemon from './data/pokemon/pokemon.js'
 console.log(Pokemon);
 
-import { filterCandy } from './data.js';
-console.log(filterCandy(Pokemon));
-//NOTA:no mover por si se no funciona de la 3 a 9
- /*  import LoL from './data/lol/lol.js a la '
-  import POTTER from './data/potter/potter.js'
-import { example } from './data.js';*/
-//*console.log(example);
-/* console.log(LoL);
- * console.log(POTTER)
-
- document.querySelector("#contenedor1").addEventListener("click", hide);
-     document.querySelector("#contenedor2").addEventListener("click", hide);
-     document.querySelector("#contenedor3").addEventListener("click", hide);
+import {filterType} from './data.js'
+//console.log();
 
 
-
-
-
-         function hide(event) {// revealed passed event argument, part of addEventListener
-           var x = event.target; //Passed clicked element to variable
-           toggleVis(x);
-           setTimeout(function() {
-             toggleVis(x);// Inserted into annon function to pass x for reveal after
-           }, 2000);
-         }
-
-         function toggleVis(target) {
-           if (target.style.display === "none") {
-             target.style.display = "block";
-           } else {
-             target.style.display = "none";
-           }
-         }*/
-         //document.getElementById('contenido').innerHTML=POKEMON.pokemon[1].name;
+let poke= filterType(Pokemon);//filterType es nuestro filter y Pokemon es la data con la que trabaja
+//console.log(poke)
+poke.map((pkm)=>{
+  //console.log(pkm)
+//let ftp = createElement('span') ya no se va a crear
+const template = `<div>${pkm.name}
+<img src="${pkm.img}"></img>
+</div>`
+//ftp.innerHTML=template;
+document.getElementById('content').innerHTML += template
+}); //En content se muesta el template
