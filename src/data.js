3697bf27c8bf79b59b979663feb2bb1ec3d08f2c
unknown <eugynias>
2019-11-26 09:57:28 -0500

    Sort

--- src/data.js
+++ src/data.js
@@ -13,19 +13,38 @@ return candy;
 
 
 //en pkm se guardan los 14 pokemones ya filtrados
-export const filterType = (Pokemon) => {
-const type= Pokemon.filter( (pkm) => {
-  for(let i=0;i<pkm.type.length;i++){
+export  const filterType = (Pokemon) => {
+const type = Pokemon.filter((pkm) => {
+  //console.log(pkm.type)
+  return pkm.type[0] === 'Grass' || pkm.type[1] === 'Grass'
+  /*for(let i=0;i<pkm.type.length;i++){
   if(pkm.type[i] === 'Grass'){
-      return pkm;
-    }
-  }
+    console.log(pkm.type[i])
+    const pokemones_ordenados = pkm.sort((pok1,pok2) => {
+      return (pok1.nombre < pok2.nombre );
+      console.log(pokemones_ordenados)
+    })
+  }*/
+//};
 });
 console.log(type)
 return type;
 };
 
+export const ordenName = (Pokemon) => {
+ const ordenNam = Pokemon.sort((pk1,pk2) => {
+   if(pk1.name > pk2.name){
+     return 1;
+   }else if(pk1.name < pk2.name){
+     return -1;
+   }
+   return 0
+})
+//console.log(ordenNam, 'order')
+return ordenNam
+ //console.log(ordenNam)
 
+};
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 
-import POKEMON from './data/pokemon/pokemon.js'
-console.log(POKEMON);
+//import POKEMON from './data/pokemon/pokemon.js'
+//console.log(POKEMON);
 
 import Pokemon from './data/pokemon/pokemon.js'
 console.log(Pokemon);
 
-import {filterType} from './data.js'
+import {filterType, ordenName} from './data.js'
 //console.log();
 
-
 let poke= filterType(Pokemon);//filterType es nuestro filter y Pokemon es la data con la que trabaja
+ordenName(poke)
 //console.log(poke)
 poke.map((pkm)=>{
  //console.log(pkm)
@@ -20,3 +20,19 @@ const template = `<div>${pkm.name}
 //ftp.innerHTML=template;
 document.getElementById('content').innerHTML += template
 }); //En content se muesta el template
+
+//function homeIcon() {
+
+  //  document.getElementById('content').style.display = "none";
+
+  //  document.getElementById('home').style.display = 'block';
+
+//}
+
+//document.getElementById('homeBtn').addEventListener('click', homeIcon);
+//funcion para cambiar a la pantalla de comparar al darle click en 'compareBtn' se encuentran los selectores
+//function compare() {
+  //  document.getElementById('home').style.display = "none"; //darle invisibilidad
+  //  document.getElementById('content').style.display = "block"; //mostrar el bloque (pantalla/slide)
+//}
+//document.getElementById('compareBtn').addEventListener('click', compare);
